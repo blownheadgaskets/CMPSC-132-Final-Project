c443603fb8d9ff7bc2d008a97d88edd28f72c5e0
Zach Mosier <zwmosier1@gmail.com>
2026-04-26 19:02:54 -0400

    got the basic implementation working, lightly tested, needs data structures and enhancements

--- Final Project.py
+++ Final Project.py
@@ -19,15 +19,48 @@ class game:
 
         if guess>self.answer: #if the guess was too large, prompt the user
             print("Too high")
+            print(" ")
         elif guess<self.answer: #if the guess was too small, prompt the user
             print('Too low')
+            print(" ")
         else: #if the guess was right, then end the game and print the appropriate messages
             print('Correct!')
             print(f"You've won the game. It took {self.attempt_num} attempts to guess the right number.")
             self.game_active = False
 
-def validate_input():
-    pass
 
+#this function handles all the input validations. It takes a guess as input and returns either true or false
+def validate_input(guess):
+    if not guess.isdigit(): #use built in method to filter out things like negatives
+        return False
+    try: #if the int conversion fails then we want to return false so the user can be prompted
+        guess = int(guess)
+    except ValueError:
+        return False
+
+    if guess <=0: #make sure that the guess is in the specified range
+        return False
+    elif guess >= 1 and guess <= 100: #only return true if all the above conditions pass and it's in the right range
+        return True
+    else: #redundant catch all
+        return False
+    
+
+#this function contains the main loop that runs while the game is being played
+#it takes no inputs and returns nothing
 def main_run_game():
-    pass
+    input('Welcome to the Number Guessing Game! Press Enter to start the game.')
+    this_game = game() # create a game object
+
+    while this_game.game_active: #continue to guess until the game ends
+        guess = input("Enter a postive integer guess from 1 to 100: ")
+        
+        if validate_input(guess):
+            guess2 =int(guess) #if the input is valid then we can convert into an int safely then pass to makeguess
+            this_game.make_guess(guess2)
+        else:
+            print("Your input is not valid, please try again.")
+
+
+if __name__ == "__main__": #this code starts a game when your run the file
+    main_run_game()
