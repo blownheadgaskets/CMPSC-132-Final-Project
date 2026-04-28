cc595345905ad970c40bec9573a0f30112e1c743
Zach Mosier <zwmosier1@gmail.com>
2026-04-27 22:29:39 -0400

    added and tested attempt limit enhancement

--- Final Project.py
+++ Final Project.py
@@ -13,8 +13,20 @@ class game:
         self.game_active = True #game is initially active
         self.difficulty = "Medium" #difficulty level of the game
         self.guess_history = [] #this list is used for keeping track of guesses, right now its only displayed in hard mode
+        self.max_attempts = 10 #this attribute sets the maximum number of attempts
 
-    
+
+    #this method checks to see if the number of attempts(guesses) has exceeded the threshold defined in init
+    #this method takes no inputs but can end the game and returns either true or false to determine what to print
+    def check_attempts(self):
+        if self.attempt_num>= self.max_attempts: #if the attempts exceeds the max, then end the game and tell the user
+            print(f"You have used all {self.max_attempts} attempts.")
+            print(f"The correct answer was {self.answer}.")
+            self.game_active = False
+            return True
+        return False
+
+
     #this method is takes a guess as input from the user. 
     #It prints hints to the user and can end the game when the right guess is found, this is medium difficulty
     def make_guess_medium_diff(self, guess):
@@ -22,10 +34,16 @@ class game:
         self.guess_history.append(guess)
         if guess>self.answer: #if the guess was too large, prompt the user
             print("Too high")
-            print(" ")
+            if not self.check_attempts(): #call check attempts to make sure that the user has not ran out of attempts
+                print(f"Attempts remaining: {self.max_attempts - self.attempt_num}")
+                print(" ") 
+
         elif guess<self.answer: #if the guess was too small, prompt the user
-            print('Too low')
-            print(" ")
+            print("Too low")
+            if not self.check_attempts():#call check attempts to make sure that the user has not ran out of attempts
+                print(f"Attempts remaining: {self.max_attempts - self.attempt_num}")
+                print(" ")
+
         else: #if the guess was right, then end the game and print the appropriate messages
             print('Correct!')
             print(f"You've won the game. It took {self.attempt_num} attempts to guess the right number in medium difficulty mode.")
@@ -51,7 +69,10 @@ class game:
                 print("You are somewhat close.")
             else:
                 print("You are far away.")
+            if not self.check_attempts(): #call check attempts to make sure that the user has not ran out of attempts
+                print(f"Attempts remaining: {self.max_attempts - self.attempt_num}")     
             print(" ")
+
         elif guess < self.answer:#if the guess was too small, prompt the user
             print("Too low")
             if difference <= hint_ranges["very close"]:
@@ -62,7 +83,10 @@ class game:
                 print("You are somewhat close.")
             else:
                 print("You are far away.")
+            if not self.check_attempts(): #call check attempts to make sure that the user has not ran out of attempts
+                print(f"Attempts remaining: {self.max_attempts - self.attempt_num}")
             print(" ")
+
         else: #if the guess was right, then end the game and print the appropriate messages
             print('Correct!')
             print(f"You've won the game. It took {self.attempt_num} attempts to guess the right number in easy mode.")
@@ -77,7 +101,10 @@ class game:
         if guess !=self.answer: #if the guess was wrong, tell the user 
             print("Your guess was wrong, try again")
             print(f"You have tried: {self.guess_history}") #display the guesses you have tried already for hard mode
+            if not self.check_attempts(): #call check attempts to make sure that the user has not ran out of attempts
+                print(f"Attempts remaining: {self.max_attempts - self.attempt_num}")
             print(" ")
+
         else: #if the guess was right, then end the game and print the appropriate messages
             print('Correct!')
             print(f"You've won the game. It took {self.attempt_num} attempts to guess the right number in hard mode.")
@@ -104,7 +131,7 @@ def validate_input(guess):
 #it takes no inputs and returns the string from the dictionary so it can be 
 def select_difficulty():
     difficulty_options = {"1": "Easy","2": "Medium","3": "Hard"} #dictionary to hold the levels
-    print("Select a difficulty: with a the associated number")
+    print("Select a difficulty level with the associated number")
     print("1. Easy")
     print("2. Medium")
     print("3. Hard")
@@ -134,6 +161,7 @@ def main_run_game():
                 this_game.make_guess_easy_diff(guess2)
             if this_game.difficulty =="Hard":
                 this_game.make_guess_hard_diff(guess2)
+
         else:
             print("Your input is not valid, please try again.")
 
